8de40562f8622e37b37339ac564b34c2c5db5de4
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-02-21 15:20:26 +0100

    arrow functions

--- Section2/play.js
+++ Section2/play.js
@@ -3,14 +3,15 @@ const name = "Thomas";
 //let : variable locale
 let age = 21;
 
+//type nom = (parametres) => {corps de la fonction}
 const resume = (userName,userAge) => {
     return(
         "Nom : " + userName +
         ", Age : " + userAge
     );
 }
-
-const add = (a,b) => a + b;
+//exemple sans parametres avec retour unique 
+const unPlusDeux = () => 1 + 2;
 
 console.log(resume(name,age));
-console.log(add(5,7));
+console.log(unPlusDeux());
