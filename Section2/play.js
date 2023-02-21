6377b78822d6ebe942291b270b348c2f7d671829
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-02-21 15:49:26 +0100

    object destructuring

--- Section2/play.js
+++ Section2/play.js
@@ -2,8 +2,7 @@
 const name = "Thomas";
 //let : variable locale
 let age = 21;
-
-//type nom = (parametres) => {corps de la fonction}
+//arrow function : type nom = (parametres) => {corps de la fonction}
 const resume = (userName,userAge) => {
     return(
         "Nom : " + userName +
@@ -16,6 +15,8 @@ const unPlusDeux = () => 1 + 2;
 console.log(resume(name,age));
 console.log(unPlusDeux());
 
+
+
 const person={
     name: "Thomas",
     age: 21,
@@ -26,11 +27,15 @@ const person={
 }
 person.fonction();
 
+
+
 const hobbies = ["Sport","Lecture"];
 //équivaut à un for (let hobby of hobbies) {...}
 console.log(hobbies);
 //.map
 console.log(hobbies.map(hobby => "Loisir : "+hobby));
+
+
 
 //spread operator : copie un par un les éléments
 const hobbiesCopy = [...hobbies];
@@ -43,10 +48,18 @@ console.log(personCopy);
 const rest = (...args) => {
     //retourne chaque élément passé en parametre
     return args;
-};
+}
 console.log(rest(1,2,3,4,5,6,7,8,9,10));
 /*
 En résumé :
     Spread : Permet de déconstruire les itérables (tableaux/objets/chaînes) en éléments uniques
     Rest: Rassemble tous les éléments restants dans un tableau
 */
+
+
+
+//object destructuring : syntaxe raccourcie
+const printName = ({ name }) => {
+    console.log(name);
+}
+printName(person);
