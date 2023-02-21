2977a08af2448693006360236d383712456bc601
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-02-21 16:34:33 +0100

    routing requests

--- Section3/app.js
+++ Section3/app.js
@@ -3,12 +3,25 @@ const http = require('http');
 
 //creation du serveur, fonction appelée par node.js  à chaque fois qu'une requete atteint le serveur
 const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
     //process.exit(); -> ferme le programme
+    if(url === '/'){
+        //écriture de contenu dans la réponse
+        res.write('<html>');
+        res.write('<head><title>Form</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Envoyer</button></form></body>');
+        res.write('</html>');
+        //fin de l'écriture
+        return res.end();
+
+    }
     //on précise au navigateur que le type d'élément renvoyé est du html
     res.setHeader('Content-Type','text/html');
     //écriture de contenu dans la réponse
-    res.write('<html><head><title>Test</title></head><body><h1>Test</h1></body></html>');
+    res.write('<html>');
+    res.write('<head><title>Section 3</title></head>');
+    res.write('<body><h1>Section 3</h1></body>');
+    res.write('</html>');
     //fin de l'écriture
     res.end();
 });
