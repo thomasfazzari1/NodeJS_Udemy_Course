8f6e441cd035e7b068fbb94663508a35af92e15f
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-02-21 15:17:34 +0100

    arrow functions

--- Section2/play.js
+++ Section2/play.js
@@ -3,11 +3,14 @@ const name = "Thomas";
 //let : variable locale
 let age = 21;
 
-function resume(userName,userAge){
+const resume = (userName,userAge) => {
     return(
         "Nom : " + userName +
         ", Age : " + userAge
     );
 }
 
+const add = (a,b) => a + b;
+
 console.log(resume(name,age));
+console.log(add(5,7));
