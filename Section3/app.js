29129bfe29c6dbad6bf13a3fa24eb1587810156c
thomasfazzari1 <fazzarithomas5@gmail.com>
2023-02-23 17:28:30 +0100

    parsing

--- Section3/app.js
+++ Section3/app.js
@@ -10,22 +10,29 @@ const server = http.createServer((req,res) => {
     if(url === '/'){
         //écriture de contenu dans la réponse
         res.write('<html>');
-        res.write('<head><title>Form</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Envoyer</button></form></body>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Envoyer</button></form></body>');
         res.write('</html>');
         //fin de l'écriture
         return res.end();
     }
 
 //33. redirecting requests
-    if(url === '/message' && req.method === 'POST'){
-        //création de fichier : (nom,contenu)
-        fs.writeFileSync('message.txt','Test');
-        res.statusCode=302;
+    if (url === '/message' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
     }
-
 
     //on précise au navigateur que le type d'élément renvoyé est du html
     res.setHeader('Content-Type','text/html');
